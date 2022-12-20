cf377ff8229cad626754ff827261ae1176a0c189
lihua <2407120445qq.com>
2022-12-20 08:53:56 +0800

    信息确认框

--- src/pages/ui/modals.js
+++ src/pages/ui/modals.js
@@ -17,6 +17,18 @@ this.setState({
   
 })
     }
+    handleConfirm=(type)=>{
+        Modal[type]({
+            title:'确认',
+            content:'你确定学会了React', 
+            onOk(){
+console.log("ok");
+            },
+            onCancel(){
+                console.log("cancle");
+            }
+        })
+    }
   render() {
   return ( 
   <div>
@@ -25,6 +37,12 @@ this.setState({
     <Button type="primary" onClick={ ()=>this.handleOpen('showModal2')}>自定义页脚</Button>
     <Button type="primary" onClick={ ()=>this.handleOpen('showModel3')}>底部20px弹窗</Button>
     <Button type="primary" onClick={ ()=>this.handleOpen('showModal4')}>水平垂直居中</Button>
+</Card>
+<Card title="信息确认框">
+    <Button type="primary" onClick={()=>this.handleConfirm('confirm')}>Confim</Button>
+    <Button type="primary" onClick={ ()=>this.handleConfirm('info')}>Info</Button>
+    <Button type="primary" onClick={ ()=>this.handleConfirm('success')}>Success</Button>
+    <Button type="primary" onClick={ ()=>this.handleConfirm('warning')}>Warning</Button>
 </Card>
 <Modal title="React"
 visible={this.state.showModal1 }
